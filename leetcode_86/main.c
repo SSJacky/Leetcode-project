59be2c2357c2a21726fe6490743889ceaa678ffa
Jacky <h0938677616@gmail.com>
2022-10-08 21:31:01 +0800

    improve 1

--- leetcode_86/main.c
+++ leetcode_86/main.c
@@ -9,10 +9,10 @@
 #include <stdint.h>
 //Definition for singly-linked list.
 struct ListNode {
-      int val;
-      struct ListNode *next;
- };
- 
+    int val;
+    struct ListNode *next;
+};
+
 
 typedef struct ListNode ListNode;
 
@@ -26,78 +26,63 @@ void print_all_ptr(ListNode *list){
     printf("N\n");
 }
 
-struct ListNode* partition(struct ListNode* head,int x){
 
+
+struct ListNode* partition(struct ListNode* head,int x){
+    
     if(head == NULL) return NULL;
-    ListNode *bigger = NULL;
-    ListNode *smaller = NULL;
     ListNode *connect_point = NULL;
     ListNode *ret=NULL;
-    ListNode *current = head;
+    ListNode **bigger = &connect_point;
+    ListNode **smaller = &ret;
     
-    while(current){
-        if(current->val<x){
-            if(smaller == NULL) {
-                smaller = current;
-                ret = smaller;
-            }
-            else {
-                smaller->next = current;
-                smaller = smaller->next;
-            }
+    while(head){
+        if(head->val<x){
+            *smaller = head;
+            smaller = &(*smaller)->next;
         }
         else{
-            if(bigger == NULL) {
-                connect_point = current;
-                bigger = current;
-            }
-            else {
-                bigger->next = current;
-                bigger = bigger->next;
-            }
+            *bigger = head;
+            bigger = &(*bigger)->next;
         }
-        current=current->next;
+        head=head->next;
     }
     
-    if(ret==NULL) return connect_point;
-    if(connect_point == NULL) return ret;
-    else smaller->next = connect_point;
-    bigger->next=NULL;
+    (*smaller) = connect_point;
+    (*bigger) = NULL;
     return ret;
 }
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     struct ListNode *headA
-        = (struct ListNode *)calloc(2, sizeof(struct ListNode));
-
+    = (struct ListNode *)calloc(6, sizeof(struct ListNode));
+    
     
     struct ListNode **p = &headA;
     (*p)->val = 1;
     (*p)->next = *p+1;
     p=&((*p)->next);
-
-    (*p)->val = 1;
-    (*p)->next = NULL;
-//    p=&((*p)->next);
-
-//    (*p)->val = 3;
-//    (*p)->next = *p+1;
-//    p=&((*p)->next);
-//
-//    (*p)->val = 2;
-//    (*p)->next = *p+1;
-//    p=&((*p)->next);
-//
-//    (*p)->val = 5;
-//    (*p)->next = *p+1;
-//    p=&((*p)->next);
-//
-//    (*p)->val = 1;
-//    (*p)->next = NULL;
-    //p=&((*p)->next);
     
-    //*p=NULL;
+    (*p)->val = 4;
+    (*p)->next = *p+1;
+    p=&((*p)->next);
+    
+    (*p)->val = 3;
+    (*p)->next = *p+1;
+    p=&((*p)->next);
+    
+    (*p)->val = 2;
+    (*p)->next = *p+1;
+    p=&((*p)->next);
+    
+    (*p)->val = 5;
+    (*p)->next = *p+1;
+    p=&((*p)->next);
+    
+    (*p)->val = 2;
+    (*p)->next = NULL;
+ 
 //    int i;
 //    for (i = 1; i <= 5; i++) {
 //        (*p)->val = i;
@@ -112,9 +97,9 @@ int main(int argc, const char * argv[]) {
         q = q->next;
     }
     printf("N\n");
-
+    
     printf("List part: ");
-    q = partition(headA,2);
+    q = partition(headA,3);
     while (q != NULL) {
         printf("%d->", q->val);
         q = q->next;
