e95d7b16282010014c713b2bbb8c16e0dd51eac7
Jacky <h0938677616@gmail.com>
2022-10-30 22:00:38 +0800

    update

--- leetcode_146/main.c
+++ leetcode_146/main.c
@@ -9,50 +9,138 @@
 #include <stdint.h>
 #include <stdbool.h>
 //Definition for singly-linked list.
-typedef struct {
+typedef struct node_t{
     int val;
-    int key;
-    struct LRUCache *prev;
-    struct LRUCache *next;
+    int valid;
+    struct node_t *next;
+    struct node_t *prev;
 } LRUCache;
 
-struct node {
-   LRUCache *node;
-}**hash_table;
+struct lru_t {
+    int size;
+    int capacity;
+    LRUCache *head;
+    LRUCache *tail;
+    LRUCache **hash_table;
+} m_lru;
 
 LRUCache* lRUCacheCreate(int capacity) {
-    LRUCache *head = malloc(sizeof(LRUCache)*capacity);
+    uint32_t index = 1;
+    
+    LRUCache *head = (LRUCache *)malloc(sizeof(LRUCache)*(capacity));
+    LRUCache **current = &head;
+    m_lru.head = head;
+    m_lru.capacity = capacity;
+    
+    (*current)->prev = NULL;
+    
+    while(index < capacity){
+        (*current)->val = 0;
+        (*current)->valid = 0;
+        (*current)->next = *current+1;
+        (*current+1)->prev = *current;
+        current = &((*current)->next);
+        index++;
+    }
+
+    (*current)->next = NULL;
+    m_lru.tail = *current;
+    m_lru.size = 0;
+    
     return head;
 }
 
 int lRUCacheGet(LRUCache* obj, int key) {
-    if(hash_table[key]->node!=obj) return 0;
-    else return 1;
+    if(m_lru.hash_table[key]->valid!=true) {
+        return 0;
+    }
+    else if(m_lru.hash_table[key]->next == m_lru.tail){
+        return 1;
+    }
+    else{
+        //LRUCache **indir = &obj;
+        LRUCache **head=obj;
+        LRUCache *current = (m_lru.hash_table[key]->prev);
+        if(current!=head){
+            current->next = m_lru.hash_table[key]->next;
+            current = current->next;
+            current->prev = m_lru.hash_table[key]->prev;
+            current = m_lru.tail->prev;
+        }
+        m_lru.hash_table[key]->next = m_lru.tail;
+        m_lru.hash_table[key]->prev = current;
+        m_lru.tail->prev = m_lru.hash_table[key];
+        current->next = m_lru.hash_table[key];
+        return 1;
+    }
 }
 
 void lRUCachePut(LRUCache* obj, int key, int value) {
-    LRUCache **head = &obj;
+    LRUCache **indir = &obj;
     LRUCache *current = obj;
-    if(*head == NULL) {
-        (*head)->val = value;
-        hash_table[key] = &(**head);
-    }
-    head = &(*head)->next;
-    hash_table[key]->node = current;
-    free(obj);
-}
 
-void lRUCacheFree(LRUCache* obj) {
+    if(m_lru.tail->prev->valid==1){
+        indir=&((*indir)->next);
+        
+        current = (*indir)->next;
+        m_lru.head->next=current;
+        current->prev = m_lru.head;
+        
+        (*indir)->prev = m_lru.tail->prev;
+        m_lru.tail->prev->next=(*indir);
+        
+        (*indir)->next = m_lru.tail;
+        
+        m_lru.hash_table[key] = (*indir);
+        (*indir)->val = value;
+        
+        return;
+        
+    }
+        
+    while((*indir)->valid == 1){
+        indir = &(*indir)->next;
+    }
+    
+    (*indir)->val=value;
+    (*indir)->valid=1;
+    m_lru.hash_table[key] = (*indir);
     
 }
 
-void print_all() {
+void lRUCacheFree(LRUCache* obj) {
+    free(obj);
+}
 
+void print_all(LRUCache* list) {
+    printf("List : ");
+    LRUCache *q = list;
+    if(q==NULL)return;
+    printf("head = %p\n", q);
+    printf("next = %p\n",q->next);
+    printf("prev = %p\n",q->prev);
+    printf("val = %d\n",q->val);
+    printf("valid = %d\n",q->valid);
+    q = q->next;
+    
+    while (q != NULL) {
+        printf("node = %p\n", q);
+        printf("next = %p\n",q->next);
+        printf("prev = %p\n",q->prev);
+        printf("val = %d\n",q->val);
+        printf("valid = %d\n",q->valid);
+        q = q->next;
+    }
+    
+    printf("N\n");
 }
 
 int main() {
     printf("first test\n");
  
+    LRUCache* head_cache=lRUCacheCreate(3);
+    
+    print_all(head_cache);
 
     printf("all tests passed!\n");
 
